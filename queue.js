771bbef1a99b58adf627bafefb534b9ed3480f1b
guillo25 <guillote25@gmail.com>
2017-03-03 15:18:24 -0300

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,4 +1,4 @@
-/*Version:2.0 Author: Guillermo Beltramino e-amil:guillote25@gmail.com*/
+/*Version:2.0 Author: Guillermo Beltramino e-mail:guillote25@gmail.com*/
 function Queue() {
   this.repetidos = true;
   if(arguments.length > 1){
